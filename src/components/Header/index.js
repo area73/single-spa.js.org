78d63b2f03208db50c90ceace267ce252b51889a
Anthony Frehner <anthony.frehner@canopytax.com>
2018-01-24 09:19:40 -0700

    We're going to call this good on the header for now.
    Mobile first, simple responsive header according to the desktop designs. Now on to rest of the page.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,13 +5,16 @@ import styles from './index.module.css'
 
 const Header = () => (
   <header className={`sspa-primary ${styles.header}`}>
-    <div className={styles.headerMobileMenuBtn}>
-      M
-    </div>
-    <Link to="/" className={styles.headerText}>
-      <img src={logoNoBg} alt="single-spa" className={styles.headerImg}/>
+    <Link to="/" className={styles.headerLogo}>
+      <img src={logoNoBg} alt="single-spa" className={styles.headerImg} height="62"/>
       <span>SINGLE-SPA</span>
     </Link>
+    <nav className={styles.headerNav}>
+      <a href="https://github.com/canopytax/single-spa">Github</a>
+      <Link to="/docs/index.html">Docs</Link>
+      <Link to="/tutorial/index.html">Getting Started</Link>
+      <a href="https://single-spa.surge.sh/">Demo</a>
+    </nav>
   </header>
 )
 
