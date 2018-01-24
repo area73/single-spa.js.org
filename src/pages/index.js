41edd03b2f3815ac46825ba26446a6a35b9ae5a3
Anthony Frehner <anthony.frehner@canopytax.com>
2018-01-24 14:15:21 -0700

    Cleaning up the index, adding css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styles from './index.module.css'
 
 const IndexPage = () => (
-  <section>
-    <h1>The javascript metaframework</h1>
+  <section className={styles.index}>
+    <h1 className={styles.h1}>The javascript metaframework</h1>
     <p>Welcome to your new Gatsby site.</p>
   </section>
 )
