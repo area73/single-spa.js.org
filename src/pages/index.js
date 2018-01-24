1eb8d974d2e24fa9593effdd303ab59ecda9016a
Anthony Frehner <anthony.frehner@canopytax.com>
2018-01-24 14:06:27 -0700

    Fixed some responsive stuff with the header, and working on the index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,10 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
+  <section>
+    <h1>The javascript metaframework</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <p>
-      <Link to="/docs/index.html">Docs</Link>
-    </p>
-    <Link to="/blog/">Blog</Link>
-  </div>
+  </section>
 )
 
 export default IndexPage
