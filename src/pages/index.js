2335056eb6b0d6368a78b04e83861b45ab222c49
Anthony Frehner <anthony.frehner@canopytax.com>
2018-01-24 16:54:51 -0700

    Adding react typist to show the frameworks.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,51 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styles from './index.module.css'
+import Typist from 'react-typist'
+import 'react-typist/dist/Typist.css'
 
-const IndexPage = () => (
-  <section className={styles.index}>
-    <h1 className={styles.h1}>The javascript metaframework</h1>
-    <p>Welcome to your new Gatsby site.</p>
-  </section>
-)
+const frameworks = [
+  'React',
+  'Angular',
+  'Vue.js',
+  'Preact',
+  'Ember',
+  'Inferno',
+  'AngularJS',
+  'Svelte',
+  'VanillaJS',
+  '...and more!'
+]
 
-export default IndexPage
+export default class IndexPage extends React.Component {
+  state = {
+    showTyping: true
+  }
+
+  render() {
+    return(
+      <section className={styles.index}>
+        <h1 className={styles.h1}>The javascript metaframework</h1>
+        <div className={styles.typing}>
+          { this.state.showTyping &&
+            <Typist startDelay={1000} avgTypingDelay={120} onTypingDone={this.onTypingDone} cursor={{element: "_"}}>
+              { frameworks.map(framework => (
+                <span key={framework}>
+                  <Typist.Delay ms={500}/>
+                  {framework}
+                  <Typist.Backspace count={framework.length} delay={1000}/>
+                </span>
+              ))}
+            </Typist>
+          }
+        </div>
+      </section>
+    )
+  }
+
+  onTypingDone = () => {
+    this.setState({showTyping: false}, () => {
+      this.setState({showTyping: true})
+    })
+  }
+}
