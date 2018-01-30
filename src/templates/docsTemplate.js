43f7e7ab12100a488573259dcad2152a210e8289
Anthony Frehner <anthony.frehner@canopytax.com>
2018-01-30 11:35:32 -0700

    Whoops, realized my mistake and returned to a function component.

--- src/templates/docsTemplate.js
+++ src/templates/docsTemplate.js
@@ -2,51 +2,50 @@ import React from "react";
 import Link from 'gatsby-link'
 import styles from './docsTemplate.module.css'
 
-export default class Template extends React.PureComponent {
-  render() {
-    const { data } = this.props // this prop will be injected by the GraphQL query below.
-    const { markdownRemark } = data; // data.markdownRemark holds our post data
-    const { frontmatter, html } = markdownRemark;
-    return (
-      <div className="sspa-container">
-        <div className="page">
-          <h1>{frontmatter.title}</h1>
-          <div
-            className="page-content"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
-          <div className={styles.footer}>
-            {frontmatter.prevLink && (
-              <span style={{marginRight: 'auto'}}>
-                &larr; &nbsp;
-                <Link to={frontmatter.prevLink}>{this.buildLinkText(frontmatter.prevLink)}</Link>
-              </span>
-            )}
-            { this.buildVersionText(frontmatter.path) }
-            {frontmatter.nextLink && (
-              <span style={{marginLeft: 'auto'}}>
-                <Link to={frontmatter.nextLink}>{this.buildLinkText(frontmatter.nextLink)}</Link>
-                &nbsp; &rarr;
-              </span>
-            )}
-          </div>
+export default function Template ({
+    data,
+}) {
+  const { markdownRemark } = data; // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <div className="sspa-container">
+      <div className="page">
+        <h1>{frontmatter.title}</h1>
+        <div
+          className="page-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+        <div className={styles.footer}>
+          {frontmatter.prevLink && (
+            <span style={{marginRight: 'auto'}}>
+              &larr; &nbsp;
+              <Link to={frontmatter.prevLink}>{buildLinkText(frontmatter.prevLink)}</Link>
+            </span>
+          )}
+          { buildVersionText(frontmatter.path) }
+          {frontmatter.nextLink && (
+            <span style={{marginLeft: 'auto'}}>
+              <Link to={frontmatter.nextLink}>{buildLinkText(frontmatter.nextLink)}</Link>
+              &nbsp; &rarr;
+            </span>
+          )}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
-  buildLinkText = link => {
-    const baseText = link.replace(/\/docs\//g, '').replace(/v\d{1}\//g, '').replace(/-/g, ' ').replace(/\.html/g, '')
-    return baseText.charAt(0).toUpperCase() + baseText.slice(1)
+function buildLinkText(link) {
+  const baseText = link.replace(/\/docs\//g, '').replace(/v\d{1}\//g, '').replace(/-/g, ' ').replace(/\.html/g, '')
+  return baseText.charAt(0).toUpperCase() + baseText.slice(1)
+}
+  
+function buildVersionText(path) {
+  const versionFound = path.match(/\/v\d{1}\//g)
+  if(versionFound) {
+    return <span style={{margin: 'auto'}}>Viewing docs for {versionFound[0].replace(/\//g, '')}</span>
   }
-
-  buildVersionText = path => {
-    const versionFound = path.match(/\/v\d{1}\//g)
-    if(versionFound) {
-      return <span style={{margin: 'auto'}}>Viewing docs for {versionFound[0].replace(/\//g, '')}</span>
-    }
-    return ''
-  }
+  return ''
 }
 
 export const pageQuery = graphql`
